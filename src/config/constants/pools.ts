54c9f4a3cc59b84fe0ff241ef373079225241824
CableGuy01 <124519285+CableGuy01@users.noreply.github.com>
2023-02-12 17:26:05 -0500

    Update pools.ts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -1,15 +1,15 @@
 import { PoolConfig, QuoteToken, PoolCategory } from './types'
 
 const pools: PoolConfig[] = [
-  {
+ {
     sousId: 0,
-    tokenName: 'SCC',
-    tokenAddress: '0xc26EaFC627624baDf990f8d30116892eD204DB51',   // token address
+    tokenName: 'LAB',
+    tokenAddress: '0x21f557ab017a9af912089e49766e90889e37fd17',   // token address
     stakingTokenName: QuoteToken.CAKE,
-    stakingTokenAddress: '0xc26EaFC627624baDf990f8d30116892eD204DB51',  // token address
+    stakingTokenAddress: '0x21f557ab017a9af912089e49766e90889e37fd17',  // token address
     contractAddress: {
       97: '',
-      56: '0x1080450b1e26Df071c613d916055b23e8FCc6Fca',  /// masterchef address
+      56: '0x81fE460CAA24e58e3A0c5CE8A4898eBe1838deD5',  /// masterchef address
     },
     poolCategory: PoolCategory.CORE,
     projectLink: '/',
@@ -19,15 +19,15 @@ const pools: PoolConfig[] = [
     isFinished: false,
     tokenDecimals: 18,
   },
-  {
+ {
     sousId: 1,
-    tokenName: 'SHIBA',
-    tokenAddress: '0xc26EaFC627624baDf990f8d30116892eD204DB51',   // token address
-    stakingTokenName: QuoteToken.CAKE,
-    stakingTokenAddress: '0xc26EaFC627624baDf990f8d30116892eD204DB51',  // token address
+    tokenName: '$flask',
+    tokenAddress: '0x11ad2281533d32a0ad16da012a4dbab846081d9b'   // token address
+    stakingTokenName: QuoteToken.$FLASK,
+    stakingTokenAddress: '$flask address',  // token address
     contractAddress: {
       97: '',
-      56: '0xa8567fa6A62bbB63DD512ECfF91df86249FE7090',
+      56: '0x03d8c7604253b33F026654a3C772B5A022e39efe',
     },
     poolCategory: PoolCategory.CORE,
     projectLink: '/',    
@@ -37,26 +37,6 @@ const pools: PoolConfig[] = [
     isFinished: false,
     tokenDecimals: 18,
   },
-   {
-    sousId: 267,
-    tokenName: 'HIGH',
-    tokenAddress: '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82',   // token address
-    stakingTokenName: QuoteToken.FORK,
-    stakingLimit: 100,
-    stakingTokenAddress: '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82',  // token address
-    contractAddress: {
-      97: '',
-      56: '0x60c4998C058BaC8042712B54E7e43b892Ab0B0c4',
-    },
-    poolCategory: PoolCategory.CORE,
-    projectLink: '/',
-    harvest: true,
-    tokenPerBlock: '0.09756',
-    sortOrder: 2,
-    isFinished: false,
-    tokenDecimals: 18,
-  },
- 
 ]
 
 export default pools
