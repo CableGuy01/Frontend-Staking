463e30c6016019c7e5250c265d86498dac53ec2e
CableGuy01 <124519285+CableGuy01@users.noreply.github.com>
2023-02-12 20:32:20 -0500

    Update pools.ts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -38,24 +38,24 @@ const pools: PoolConfig[] = [
     isFinished: true,
     tokenDecimals: 18,
   },
-//  {
-//     sousId: 1,
-//     tokenName: 'FLASK',
-//     tokenAddress: '0x11ad2281533d32a0ad16da012a4dbab846081d9b'   // token address
-//     stakingTokenName: QuoteToken.FLASK,
-//     stakingTokenAddress: '0x11ad2281533d32a0ad16da012a4dbab846081d9b',  // token address
-//     contractAddress: {
-//       97: '',
-//       56: '0x03d8c7604253b33F026654a3C772B5A022e39efe',
-//     },
-//     poolCategory: PoolCategory.CORE,
-//     projectLink: '/',    
-//     harvest: true,
-//     tokenPerBlock: '10',
-//     sortOrder: 2,
-//     isFinished: false,
-//     tokenDecimals: 18,
-//   },
+ {
+    sousId: 1,
+    tokenName: 'FLASK',
+    tokenAddress: '0x11ad2281533d32a0ad16da012a4dbab846081d9b'   // token address
+    stakingTokenName: QuoteToken.FLASK,
+    stakingTokenAddress: '0x11ad2281533d32a0ad16da012a4dbab846081d9b',  // token address
+    contractAddress: {
+      97: '',
+      56: '0x03d8c7604253b33F026654a3C772B5A022e39efe',
+    },
+    poolCategory: PoolCategory.CORE,
+    projectLink: '/',    
+    harvest: true,
+    tokenPerBlock: '10',
+    sortOrder: 2,
+    isFinished: false,
+    tokenDecimals: 18,
+  },
 ]
 
 export default pools
